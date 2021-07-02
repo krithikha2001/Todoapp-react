ae832d08988c2422f61070f6f53637f9a2263eac
krithikha2001 <krithikhabala@gmail.com>
2021-07-02 13:26:04 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
@@ -8,10 +9,15 @@ function App() {
   const [todos, setTodo] = useState([]);
   const [status, setStatus] = useState("all");
   const [fstat, setFstat] = useState([]);
+  //runsonly once
+  useEffect(() => {
+    getLocalData();
+  }, []);
   //runs only once when rendered and rerun by adding a value if u want...here submit
   useEffect(() => {
     //   console.log("sds");
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
   //function
   const filterHandler = () => {
@@ -29,6 +35,19 @@ function App() {
     }
   };
   //filter happens when item added and in filter option wise too
+  //connecting with local storage to store whole session
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const getLocalData = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let Todoloc = JSON.parse(localStorage.getItem("todos"));
+      //  saveLocalTodos(Todoloc);
+      setTodo(Todoloc);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,4 @@
 import react from "react";
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom";
 
 const Todo = ({ text, td, std, x }) => {
   //events
